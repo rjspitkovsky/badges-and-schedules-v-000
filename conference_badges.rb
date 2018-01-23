75e03d447805ddbf1f402ee54b9c0c104e44c8ba
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 19:19:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,11 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  
+  rooms = []
+  count = 1
+  attendees.each do |name|
+    rooms << "Hello, #{name}! You'll be assigned to room #{count}!"
+    count += 1
+  end
+  rooms 
 end
