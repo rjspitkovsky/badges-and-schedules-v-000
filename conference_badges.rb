762d01a91ee91949f151389a2f24d2734a264e09
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-01-23 19:16:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creater(attendees)
+def batch_badge_creator(attendees)
   attendees.each do |attendee|
     "Hello, my name is #{attendee}."
-  end 
+  end
 end
